2718d39d5c04b481774f4698ef80c571ec1b481b
waleedsaifullah <waleedsaifullah786@gmail.com>
2023-05-06 22:30:47 +0500

    project created

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import Navbar from "@/components/Navbar"
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home")
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    </div>
+  )
+}
+
+export default App
